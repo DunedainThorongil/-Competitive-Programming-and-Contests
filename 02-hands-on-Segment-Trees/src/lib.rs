f40a638afd113cf08d76335485af841b40d5f2d0
Alessandrea De Lucrezia <a.delucrezia@studenti.unipi.ti>
2023-11-25 20:18:52 +0100

    feat: added metod for: query, update, build a segment tree

---
+++ 02-hands-on-Segment-Trees/src/lib.rs
@@ -0,0 +1,72 @@
+use std::cmp::{max, min};
+
+pub struct SegmentTree{
+    pub tree: Vec<i32>,
+    pub lazy: Vec<i32>
+}
+
+impl SegmentTree{
+    pub fn build_segment_tree(&mut self, array: Vec<i32>, len_array: i32, v: i32, tl: i32, tr: i32){
+        self.tree= vec![0; (len_array * 4) as usize];
+        self.lazy= vec![0; (len_array * 4) as usize];
+        self.build(&array, v, tl, tr);
+    }
+
+    fn build(&mut self, array: &Vec<i32>, v: i32, tl: i32, tr: i32){
+        if tl == tr {
+            self.tree[v as usize] = array[tl as usize]
+        } else {
+            let tm: i32 = (tl + tr) / 2;
+            self.build(array, v*2, tl, tm);
+            self.build(array, v*2+1, tm+1, tr);
+            self.tree[v as usize] = max(self.tree[(v*2) as usize] , self.tree[(v*2 +1) as usize]);
+        }
+    }
+
+    pub fn update(&mut self, v: i32, tl: i32, tr: i32, l: i32, r: i32, value: i32){
+        self.update_range(v, tl, tr, l, r, value);
+    }
+    fn update_range(&mut self, v: i32, tl: i32, tr: i32, l: i32, r: i32, value:i32){
+        if l > r {
+            return;
+        }
+        if l == tl && tr == r {
+            self.tree[v as usize] += min(self.tree[v as usize], value);
+            self.lazy[v as usize] += min(self.lazy[v as usize], value);
+        } else {
+            self.push(v);
+            let tm = (tl + tr) / 2;
+            self.update( v*2, tl, tm, l, min(r, tm), value);
+            self.update(v*2 +1, tm+1, tr, max(l, tm+1), r, value);
+            self.tree[v as usize] = max(self.tree[(v*2) as usize], self.tree[(v*2+1) as usize]);
+        }
+    }
+
+    pub fn push(&mut self, v: i32){
+        self.push_update(v)
+    }
+    fn push_update(&mut self, v: i32){
+        self.tree[(v*2) as usize] = min(self.tree[(v*2) as usize],self.lazy[v as usize]);
+        self.lazy[(v*2) as usize] = min( self.lazy[(v*2) as usize],self.lazy[v as usize]);
+        self.tree[(v*2+1) as usize] = min( self.tree[(v*2+1) as usize],self.lazy[v as usize] );
+        self.lazy[(v*2+1) as usize] = min(self.lazy[(v*2+1) as usize], self.lazy[v as usize]);
+        self.lazy[v as usize] = 0;
+    }
+
+    pub fn query(&mut self, v: i32, tl: i32, tr: i32, l: i32, r: i32 ) -> i32 {
+        self.query_max(v, tl, tr, l, r)
+    }
+    fn query_max(&mut self, v: i32, tl: i32, tr: i32, l: i32, r: i32) -> i32 {
+        if l > r {
+            return -1;
+        }
+        if l== tl && tr == r {
+            return self.tree[v as usize];
+        }
+        self.push(v);
+        let tm = (tl + tr) / 2;
+        return max(self.query_max(self.tree[(v*2) as usize], tl, tm, l, min(r, tm)),
+                   self.query_max(self.tree[(v*2+1) as usize], tm+1, tr, max(l, tm+1), r))
+
+    }
+}
---
+++ 02-hands-on-Segment-Trees/src/main.rs
@@ -0,0 +1,55 @@
+mod libs;
+use libs::SegmentTree;
+
+use std::path::Path;
+use std::fs::{ File};
+use std::io::{ BufReader, BufRead};
+
+fn read(file_pat: String) {
+    let mut tree :SegmentTree = SegmentTree{ tree: vec![], lazy: vec![] };
+    let file = File::open(file_pat).expect("Impossibile leggere il file");
+    let mut reader = BufReader::new(file);
+
+    let mut conta_linea = 0;
+    let mut dim_array: i32 = 0;
+    let mut n_query: i32 = 0;
+
+
+    for line in reader.lines() {
+        conta_linea = conta_linea + 1;
+        let value= line.expect("REASON");
+        println!("Linea letta: {}", value);
+        let value_split: Vec<&str> = value.split(' ').collect();
+        if conta_linea == 1 {
+            dim_array = value_split[0].parse().unwrap();
+            n_query = value_split[1].parse().unwrap();
+        }
+        else if conta_linea == 2 {
+            let array: Vec<i32> = value_split.iter().map(|&s| s.parse().unwrap()).collect();
+            tree.build_segment_tree(array,dim_array, 1, 0, dim_array-1);
+        }
+        else if conta_linea > 2 {
+            let value_split_int: Vec<i32> = value_split.iter().map(|&s| s.parse().unwrap()).collect();
+            if value_split_int[0] == 0 { // Update (i, j, k)
+                let i: i32 = value_split_int[1] ;
+                let j: i32 = value_split_int[2] ;
+                let k: i32 = value_split_int[3] ;
+            }
+            else if value_split_int[0] == 1 { // Max(i, j)
+                let i: i32 = value_split_int[1];
+                let j: i32 = value_split_int[2];
+                println!("Risulato query Max: {}", tree.query(1, 0, dim_array-1, i, j))
+
+            }
+        }
+    }
+}
+
+fn main() {
+    let file_path = "02-hands-on-Segment-Trees/src/Testset_handson2_2324_p1/input0.txt";
+    let exists = Path::new(file_path).exists();
+    if exists {
+        read(file_path.to_string());
+    }
+
+}
