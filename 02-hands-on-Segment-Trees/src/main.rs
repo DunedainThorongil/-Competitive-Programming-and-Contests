77e4d485dcc239daa7e98294afc5f225f5eaa912
Alessandrea De Lucrezia <a.delucrezia@studenti.unipi.ti>
2023-11-26 22:29:36 +0100

    feat: added checking of all tests by reading files from the tests directory

--- 02-hands-on-Segment-Trees/src/main.rs
+++ 02-hands-on-Segment-Trees/src/main.rs
@@ -1,4 +1,6 @@
 mod libs;
+
+use std::fs;
 use libs::SegmentTree;
 
 use std::path::Path;
@@ -7,26 +9,24 @@ use std::io::{ BufReader, BufRead};
 
 fn read(file_pat: String) {
     let mut tree :SegmentTree = SegmentTree{ tree: vec![], lazy: vec![] };
-    let file = File::open(file_pat).expect("Impossibile leggere il file");
+    let file = File::open(file_pat).expect("Unable to read the input file");
     let mut reader = BufReader::new(file);
 
     let mut conta_linea: i32= 0;
     let mut dim_array: usize = 0;
-    let mut n_query: usize = 0;
-
-
+    
     for line in reader.lines() {
         conta_linea = conta_linea + 1;
         let value= line.expect("REASON");
-        println!("Linea letta: {}", value);
+        //println!("Linea letta: {}", value);
         let value_split: Vec<&str> = value.split(' ').collect();
         if conta_linea == 1 {
             dim_array = value_split[0].parse().unwrap();
-            n_query = value_split[1].parse().unwrap();
+            tree.new_segment_tree(dim_array);
         }
         else if conta_linea == 2 {
             let array: Vec<usize> = value_split.iter().map(|&s| s.parse().unwrap()).collect();
-            tree.build(array, 1, 0, dim_array-1);
+            tree.build(&array, 1, 0, dim_array-1);
         }
         else if conta_linea > 2 {
             let value_split_int: Vec<usize> = value_split.iter().map(|&s| s.parse().unwrap()).collect();
@@ -35,12 +35,15 @@ fn read(file_pat: String) {
                 let j: usize = value_split_int[2] ;
                 let k: usize = value_split_int[3] ;
                 tree.update(1, 0, dim_array-1, i-1, j-1, k);
-                println!("Query Update!");
+                //println!("Query Update!");
             }
             else if value_split_int[0] == 1 { // Max(i, j)
                 let i: usize = value_split_int[1];
                 let j: usize = value_split_int[2];
-                println!("Response query Max: {}", tree.query(1, 0, dim_array-1, i-1, j-1))
+                let risp = tree.query(1, 0, dim_array-1, i-1, j-1);
+                response_array.push(risp);
+                //println!("{}", risp)
+                //println!("Response query Max: {}", tree.query(1, 0, dim_array-1, i-1, j-1))
 
             }
         }
@@ -48,10 +51,46 @@ fn read(file_pat: String) {
 }
 
 fn main() {
-    let file_path = "02-hands-on-Segment-Trees/src/Testset_handson2_2324_p1/input0.txt";
-    let exists = Path::new(file_path).exists();
-    if exists {
-        read(file_path.to_string());
+    let directory_path = "src/Testset_handson2_2324_p1/";
+    if let Ok(entries) = fs::read_dir(directory_path) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                //println!("Sono dentro la directory");
+                let file_name = entry.file_name();
+                let file_name_str = file_name.to_string_lossy();
+                if file_name_str.starts_with("input") {
+                    let input_file_path: String = format!("{}{}",directory_path, file_name_str);
+                    let exists = Path::new(input_file_path.as_str()).exists();
+
+                    if exists {
+                        let risp = read(input_file_path.to_string());
+                        let mut output_correct: Vec<usize> = Vec::new();
+                        let output_file_path = input_file_path.replace("input", "output");
+                        let exits_output_file = Path::new(&output_file_path).exists();
+
+                        if exits_output_file {
+                            let file = File::open(&output_file_path).expect("Unable to read the output file");
+                            let reader = BufReader::new(file);
+                            for line in reader.lines() {
+                                output_correct.push(line.expect("REASON").parse().unwrap());
+                            }
+                        }
+
+                        if risp == output_correct {
+                            println!("Test passed {}", input_file_path);
+                        } else {
+                            println!("Test NOT passed {}", input_file_path);
+                        }
+                    }
+
+                    //println!("File trovato: {}", file_name_str);
+                }
+            } else {
+                println!("Error reading directory: {}", directory_path);
+            }
+        }
+
     }
+
 
 }
